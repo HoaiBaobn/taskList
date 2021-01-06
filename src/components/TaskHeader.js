8bbf54d8fcab5e057bf7eba527199599f7e93988
hoai bao <nguyenthihoaibn96@gmail.com>
2021-01-06 21:54:47 +0700

    add input

--- src/components/TaskHeader.js
+++ src/components/TaskHeader.js
@@ -1,5 +1,14 @@
 const TaskHeader = () => {
-    return <h1>Tasks</h1>;
+    return (
+        <header class="header">
+            <h1>Tasks</h1>
+            <input
+                type="text"
+                class="new-task"
+                placeholder="What needs to be done?"
+            />
+        </header>
+    );
 };
 
 export default TaskHeader;
